42f64c88abdf89ef546144e8511f7f5fe5a6c058
Prakash-hg <99275271+Prakash-hg@users.noreply.github.com>
2022-09-15 13:02:47 +0530

    Update Greeter.java
    edit

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,6 +11,46 @@ public class Greeter {
   public Greeter() {
 
   }
+  public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+    public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+      public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+        public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+          public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
